8bc0580cb13d51ef6aded23c374dd7bdcc169ee4
Saurabh Warungse <saurabhwarungse4050@gmail.com>
2025-10-19 14:48:42 +0530

    change

---
+++ src/ex_02_Keywords_Identifier_Variables/Lab008_Dynamically_typed.py
@@ -0,0 +1,11 @@
+#Python is dynamically typed language it means we can data type of predefined  variable
+
+name = "saurabh"
+print(name)
+print(type(name))
+
+name = 123
+print(name)
+print(type(name))
+
+
