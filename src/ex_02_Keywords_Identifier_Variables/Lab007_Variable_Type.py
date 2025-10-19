2bb7601dd514d1561c9d5749e20656fd9989cf62
Saurabh Warungse <saurabhwarungse4050@gmail.com>
2025-10-19 14:26:45 +0530

    change

---
+++ src/ex_02_Keywords_Identifier_Variables/Lab007_Variable_Type.py
@@ -0,0 +1,12 @@
+_name = "saurabh"   # String
+_123 = 123          #int
+pi = 3.14           #flot
+istrue = True       #boolean
+
+#type command is used to find the data type of variable
+
+print(type(_name))
+print(type(_123))
+print(type(pi))
+print(type(istrue))
+
